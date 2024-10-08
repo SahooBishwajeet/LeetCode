149cfc41153777f485c6182918b22eff434fc425
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-10-08 09:40:48 +0530

    Add Another Solution to 1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp

--- Solutions_CPP/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp
+++ Solutions_CPP/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp
@@ -1,8 +1,8 @@
 // 1963 : Minimum Number of Swaps to Make the String Balanced
 
 /*
-You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
-A string is called balanced if and only if:
+You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening
+brackets '[' and n / 2 closing brackets ']'. A string is called balanced if and only if:
 
 It is the empty string, or
 It can be written as AB, where both A and B are balanced strings, or
@@ -10,7 +10,7 @@ It can be written as [C], where C is a balanced string.
 
 You may swap the brackets at any two indices any number of times.
 Return the minimum number of swaps to make s balanced.
- 
+
 
 */
 
@@ -37,7 +37,7 @@ Input: s = "[]"
 Output: 0
 Explanation: The string is already balanced.
 
- 
+
 
 */
 
@@ -48,7 +48,8 @@ n == s.length
 2 <= n <= 106
 n is even.
 s[i] is either '[' or ']'.
-The number of opening brackets '[' equals n / 2, and the number of closing brackets ']' equals n / 2.
+The number of opening brackets '[' equals n / 2, and the number of closing brackets ']' equals n
+/ 2.
 
 
 */
@@ -56,24 +57,27 @@ The number of opening brackets '[' equals n / 2, and the number of closing brack
 /*
 - We can solve this problem by using stack or an simulation of stack.
 
-- Any time we encoutner an opening bracket, we push it to the stack. (i.e. we increment the number of open brackets)
-- Else (when we encoutner a closing bracket), we check if there are any open brackets in the stack. If yes, we pop it from the stack. (i.e. we decrement the number of open brackets)
-- If there are no open brackets to be matched with the current closing bracket, we increment the number of close brackets.
+- Any time we encoutner an opening bracket, we push it to the stack. (i.e. we increment the number
+of open brackets)
+- Else (when we encoutner a closing bracket), we check if there are any open brackets in the stack.
+If yes, we pop it from the stack. (i.e. we decrement the number of open brackets)
+- If there are no open brackets to be matched with the current closing bracket, we increment the
+number of close brackets.
 
 - In the end, the total unmatched brackets' count is taken.
 - To fix 2 unmatched brackets, we need 1 swap. So, we return (total unmatched brackets + 1) / 2
 */
 
 class Solution {
-public:
+   public:
     int minSwaps(string s) {
         int open = 0, close = 0;
 
-        for(auto ch : s) {
-            if(ch == '[') {
+        for (auto ch : s) {
+            if (ch == '[') {
                 open++;
             } else {
-                if(open > 0) {
+                if (open > 0) {
                     open--;
                 } else {
                     close++;
@@ -82,6 +86,26 @@ public:
         }
 
         int maxOC = max(open, close);
-        return (maxOC+1)/2;
+        return (maxOC + 1) / 2;
+    }
+};
+
+class Solution {
+   public:
+    int minSwaps(string s) {
+        stack<char> st;
+        int cnt = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '[') {
+                st.push(s[i]);
+            } else {
+                if (!st.empty()) {
+                    st.pop();
+                } else {
+                    cnt++;
+                }
+            }
+        }
+        return (cnt + 1) / 2;
     }
 };
